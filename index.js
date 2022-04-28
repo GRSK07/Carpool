d516974ae539afb93f28dee60e6e6d80b38f4850
GRSK07 <87566600+GRSK07@users.noreply.github.com>
2022-04-28 17:27:41 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+function loginForm(){
+    document.getElementById("form2").style.display="block"
+    document.getElementById("form1").style.display="none"
+}
+function signUpForm(){
+    document.getElementById("form1").style.display="block"
+    document.getElementById("form2").style.display="none"
+}
